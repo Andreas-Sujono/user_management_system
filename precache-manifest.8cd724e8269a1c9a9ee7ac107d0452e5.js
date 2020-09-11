ace592203a54a0950917e764653443532c88961f
Andreas_Sujono <andreassujono@gmail.com>
2020-09-11 18:28:39 +0800

    Updates

---
+++ precache-manifest.8cd724e8269a1c9a9ee7ac107d0452e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d55960047a71b90cdcb6b4c7e7edae5",
+    "url": "/user_management_system/index.html"
+  },
+  {
+    "revision": "4ec0e08ef3a0d93118f8",
+    "url": "/user_management_system/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "fe663bc1b3016968b66d",
+    "url": "/user_management_system/static/css/main.3f404232.chunk.css"
+  },
+  {
+    "revision": "4ec0e08ef3a0d93118f8",
+    "url": "/user_management_system/static/js/2.19da8663.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/user_management_system/static/js/2.19da8663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe663bc1b3016968b66d",
+    "url": "/user_management_system/static/js/main.0163a32d.chunk.js"
+  },
+  {
+    "revision": "5eef65d767de523378ef",
+    "url": "/user_management_system/static/js/runtime-main.cc1a76b8.js"
+  }
+]);
