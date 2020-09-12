ecafbf9d8996f11c1711c03cc8ea6f93740ff885
Andreas_Sujono <andreassujono@gmail.com>
2020-09-12 12:20:56 +0800

    Updates

---
+++ precache-manifest.26f8b438984a22b3575bbc35199da843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e67eafffd8a1da11316bd4898ba4015",
+    "url": "/user_management_system/index.html"
+  },
+  {
+    "revision": "174b4cc9c9ba40764d16",
+    "url": "/user_management_system/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "523113d3a318e900c0d7",
+    "url": "/user_management_system/static/css/main.d786e012.chunk.css"
+  },
+  {
+    "revision": "174b4cc9c9ba40764d16",
+    "url": "/user_management_system/static/js/2.6e7ce0a7.chunk.js"
+  },
+  {
+    "revision": "8aa5ce1e30c532c6aca185a23a60927f",
+    "url": "/user_management_system/static/js/2.6e7ce0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523113d3a318e900c0d7",
+    "url": "/user_management_system/static/js/main.cf2a27d7.chunk.js"
+  },
+  {
+    "revision": "5eef65d767de523378ef",
+    "url": "/user_management_system/static/js/runtime-main.cc1a76b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/user_management_system/precache-manifest.f4cf5bf9d26cf2e71933f6a2a3b88136.js"
+  "/user_management_system/precache-manifest.26f8b438984a22b3575bbc35199da843.js"
 );
 
 self.addEventListener('message', (event) => {
